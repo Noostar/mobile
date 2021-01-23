572f97f9aef7a5aef38e015705dbc6800b083958
irinache <irina.che333@gmail.com>
2021-01-23 21:58:43 +0200

    Updated course_list
    - Wrap widget replaced with GridView
    - Created a separate widget for course block

--- lib/examples_from_courses/presentation/pages/course_list/course_list.dart
+++ lib/examples_from_courses/presentation/pages/course_list/course_list.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
 
-const List<String> courses = [
-  'Course 1',
-  'Course 2',
-  'Course 3',
-  'Course 4',
-  'Course 5',
-  'Course 6'
-];
-
 class CourseListPage extends StatelessWidget {
   const CourseListPage({Key key}) : super(key: key);
 
@@ -41,13 +32,7 @@ class CourseListPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 21),
                 const Expanded(
-                  child: SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    padding: EdgeInsets.only(
-                      bottom: 75,
-                    ),
-                    child: _CourseList(),
-                  ),
+                  child: _CourseList(),
                 ),
               ],
             ),
@@ -60,44 +45,71 @@ class _CourseList extends StatelessWidget {
   const _CourseList({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Wrap(
-        runSpacing: 21,
-        spacing: 22,
-        children: <Widget>[
-          for (final course in courses)
-            Stack(
-              alignment: AlignmentDirectional.bottomStart,
-              children: <Widget>[
-                Container(
-                  height: 167,
-                  width: 167,
-                  color: const Color(0xFFC4C4C4),
-                  alignment: Alignment.bottomLeft,
-                  padding: const EdgeInsets.only(
-                    bottom: 15,
-                    left: 6,
-                  ),
-                  child: Text(
-                    course,
-                    style: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
+  Widget build(BuildContext context) => GridView.count(
+        primary: false,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 11,
+        crossAxisCount: 2,
+        padding: const EdgeInsets.only(
+          bottom: 75,
+        ),
+        children: List.generate(
+          6,
+          (index) => _CourseListItem(
+            title: 'Course $index',
+            progress: const Percent(value: 100),
+          ),
+        ),
+      );
+}
+
+class _CourseListItem extends StatelessWidget {
+  final String title;
+  final Percent progress;
+  const _CourseListItem({Key key, this.title, this.progress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: <Widget>[
+            Container(
+              height: 167,
+              width: 167,
+              color: const Color(0xFFC4C4C4),
+              alignment: Alignment.bottomLeft,
+              padding: const EdgeInsets.only(
+                bottom: 15,
+                left: 6,
+              ),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xFFFFFFFF),
                 ),
-                Container(
-                  height: 11,
-                  width: 167,
-                  color: const Color(0xFF81c54b),
-                  child: const Text(
-                    '100%',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 10),
-                  ),
-                ),
-              ],
-            )
-        ],
+              ),
+            ),
+            Container(
+              height: 11,
+              width: 167,
+              color: const Color(0xFF81c54b),
+              child: progress,
+            ),
+          ],
+        ),
+      );
+}
+
+class Percent extends StatelessWidget {
+  final int value;
+  const Percent({Key key, this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Text(
+        '$value%',
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 10),
       );
 }
