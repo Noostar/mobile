b5f29309cfc23b4881ddd33c1a0012ceffdfc276
Xdantex0000 <eugenzaginaylo@gmail.com>
2021-01-27 14:40:13 +0200

    Fix problems

--- lib/examples_from_courses/presentation/pages/course_details/recovery_password.dart
+++ lib/examples_from_courses/presentation/pages/course_details/recovery_password.dart
@@ -8,65 +8,60 @@ class RecoveryPassword extends StatelessWidget {
         child: SafeArea(
           child: Scaffold(
             body: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width *
-                      0.13), // * 13% Padding of the screen
+              padding: const EdgeInsets.symmetric(horizontal: 35),
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            '''
-Password
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '''Password
 recovery:''',
-                            style: TextStyle(fontSize: 40),
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: const Opacity(
-                              opacity: 0.6,
-                              child: Text(
-                                'Back',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ),
-                          ),
-                        ]),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    const Text(
-                      'Email:',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const TextField(
-                        decoration: InputDecoration(
-                            hintText: 'Write your email here',
-                            fillColor: Color(0xffC4C4C4))),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                        color: const Color(0xff565DFF),
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      TextButton(
                         onPressed: () {},
-                        child: const Padding(
-                          padding: EdgeInsets.fromLTRB(21, 9, 21, 9),
+                        child: const Opacity(
+                          opacity: 0.6,
                           child: Text(
-                            'Submit',
-                            style: TextStyle(color: Colors.white, fontSize: 30),
+                            'Back',
+                            style: TextStyle(fontSize: 30, color: Colors.black),
                           ),
                         ),
                       ),
-                    )
-                  ]),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  const Text(
+                    'Email:',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  const SizedBox(height: 10),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Write your email here',
+                      fillColor: Color(0xffC4C4C4),
+                    ),
+                  ),
+                  const SizedBox(height: 60),
+                  Align(
+                    alignment: Alignment.center,
+                    child: RaisedButton(
+                      color: const Color(0xff565DFF),
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.fromLTRB(21, 9, 21, 9),
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
