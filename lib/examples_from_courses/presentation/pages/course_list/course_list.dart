3922e056a2f4a4a2bdea3746380ba300f2d73ba5
irinache <irina.che333@gmail.com>
2021-04-15 14:07:01 +0300

    Update course_list.dart
    - Moved big parts of code to separate widgets.
    - Created map for tabs.
    - Moved left and right padding from parent widget to child widgets.
    - Added small improvements.

--- lib/examples_from_courses/presentation/pages/course_list/course_list.dart
+++ lib/examples_from_courses/presentation/pages/course_list/course_list.dart
@@ -8,105 +8,41 @@ class CourseListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Color(0xFFF1F0F6),
-    ));
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Color(0xFFF1F0F6),
+        statusBarBrightness: Brightness.light,
+      ),
+    );
+    final tabs = {
+      const Tab(text: 'Active'): const _CourseList(),
+      const Tab(text: 'Recomended'): const _CourseList(),
+      const Tab(text: 'Available'): const _CourseList(),
+    };
     return DefaultTabController(
-      length: 3,
+      length: tabs.keys.length,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(85.0),
-          child: AppBar(
-            titleSpacing: 0,
-            brightness: Brightness.light,
-            backgroundColor: const Color(0xFFF1F0F6),
-            title: Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-              child: Row(
-                children: [
-                  Container(
-                    width: 35,
-                    height: 35,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/avatar.png'),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 13,
-                  ),
-                  const Text(
-                    'Katherine',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF0A063C),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            elevation: 0,
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+        body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'Courses',
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF0A063C),
+              const UserInfo(),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Courses',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF0A063C),
+                  ),
                 ),
               ),
               const SizedBox(height: 14),
-              Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 1,
-                    color: const Color(0xFFD7D6E4),
-                    margin: const EdgeInsets.only(bottom: 2),
-                  ),
-                  const TabBar(
-                    isScrollable: true,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicatorWeight: 5,
-                    indicatorColor: Color(0xFFFF5215),
-                    unselectedLabelColor: Color(0xFF8886A9),
-                    labelColor: Color(0xFFFF5215),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    tabs: [
-                      Tab(
-                        text: 'Active',
-                      ),
-                      Tab(
-                        text: 'Recommended',
-                      ),
-                      Tab(
-                        text: 'Available',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              CourseTabs(tabs: tabs),
               const SizedBox(height: 21),
-              const Expanded(
-                child: TabBarView(children: [
-                  _CourseList(),
-                  _CourseList(),
-                  _CourseList(),
-                ]),
+              Expanded(
+                child: TabBarView(children: tabs.values.toList()),
               ),
             ],
           ),
@@ -136,6 +72,7 @@ class _CourseList extends StatelessWidget {
         }
 
         return ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           itemBuilder: (BuildContext context, int index) {
             if (index >= snapshot.data.courseItems.length) {
               if (!snapshot.data.isLoading) {
@@ -145,6 +82,7 @@ class _CourseList extends StatelessWidget {
                 child: Padding(
                   padding: EdgeInsets.only(
                     top: 20,
+                    bottom: 20,
                   ),
                   child: SizedBox(
                     height: 24,
@@ -163,9 +101,6 @@ class _CourseList extends StatelessWidget {
             );
           },
           itemCount: itemCount,
-          padding: const EdgeInsets.only(
-            bottom: 75,
-          ),
         );
       },
     );
@@ -198,13 +133,15 @@ class _CourseListItem extends StatelessWidget {
                 Container(
                   height: 224,
                   decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: FractionalOffset.center,
-                          end: FractionalOffset.bottomCenter,
-                          colors: [
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.center,
+                      end: FractionalOffset.bottomCenter,
+                      colors: [
                         Color(0x00000000),
                         Color(0xD5000000),
-                      ])),
+                      ],
+                    ),
+                  ),
                 ),
                 Container(
                   padding: const EdgeInsets.only(
@@ -236,60 +173,153 @@ class _CourseListItem extends StatelessWidget {
                     ],
                   ),
                 ),
-                Stack(
-                  children: [
-                    Container(
-                      height: 14,
-                      width: double.infinity,
-                      color: const Color(0x59626262),
-                    ),
-                    Container(
-                      height: 14,
-                      width: double.infinity,
-                      alignment: Alignment.centerLeft,
-                      child: FractionallySizedBox(
-                        widthFactor: progress.getValue() * 0.01,
-                        child: Container(
-                          width: 140,
-                          height: 14,
-                          decoration: BoxDecoration(
-                            borderRadius: (progress.getValue() != 100)
-                                ? const BorderRadius.only(
-                                    topRight: Radius.circular(50),
-                                    bottomRight: Radius.circular(50),
-                                  )
-                                : null,
-                            color: (() {
-                              if (progress.getValue() <= 33)
-                                return const Color(0xFFF2C94C);
-                              else if (progress.getValue() > 33 &&
-                                  progress.getValue() <= 66)
-                                return const Color(0xFFB1D64B);
-                              else if (progress.getValue() > 66)
-                                return const Color(0xFF239621);
-                            })(),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 14,
-                      width: double.infinity,
-                      child: Text(
-                        progress.getPercent(),
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 10,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                CourseProgressBar(progress: progress),
               ],
             ),
           ),
+        ),
+      );
+}
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 20,
+            bottom: 25,
+            left: 20,
+            right: 20,
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 35,
+                height: 35,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/avatar.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              const SizedBox(
+                width: 13,
+              ),
+              const Text(
+                'Katherine',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF0A063C),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+}
+
+class CourseTabs extends StatelessWidget {
+  final Map tabs;
+  const CourseTabs({Key key, this.tabs}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned(
+            left: 0,
+            bottom: 2,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 1,
+              color: const Color(0xFFD7D6E4),
+            ),
+          ),
+          TabBar(
+            isScrollable: true,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 5,
+            indicatorColor: Color(0xFFFF5215),
+            unselectedLabelColor: Color(0xFF8886A9),
+            labelColor: Color(0xFFFF5215),
+            labelStyle: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+            tabs: tabs.keys.toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseProgressBar extends StatelessWidget {
+  final Percent progress;
+  const CourseProgressBar({Key key, this.progress}) : super(key: key);
+
+  Color getColor(Percent progress) {
+    if (progress.getValue() <= 33) {
+      return const Color(0xFFF2C94C);
+    } else if (progress.getValue() > 33 && progress.getValue() <= 66) {
+      return const Color(0xFFB1D64B);
+    } else if (progress.getValue() > 66) {
+      return const Color(0xFF239621);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: Stack(
+          children: [
+            Container(
+              height: 14,
+              width: double.infinity,
+              color: const Color(0x59626262),
+            ),
+            Container(
+              height: 14,
+              width: double.infinity,
+              alignment: Alignment.centerLeft,
+              child: FractionallySizedBox(
+                widthFactor: progress.getValue() * 0.01,
+                child: Container(
+                  width: 140,
+                  height: 14,
+                  decoration: BoxDecoration(
+                    borderRadius: (progress.getValue() != 100)
+                        ? const BorderRadius.only(
+                            topRight: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                          )
+                        : null,
+                    color: getColor(progress),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 14,
+              width: double.infinity,
+              child: Text(
+                progress.getPercent(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 10,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
         ),
       );
 }
