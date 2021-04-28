52aea5307c1adcf682f3a8b2cc0bf44369ed945c
Denis Taranenko <denis.taranenko@windmill.ch>
2021-04-28 20:29:45 +0300

    Fixed issue with logo.

--- lib/examples_from_courses/presentation/pages/sign_in/sign_in_form.dart
+++ lib/examples_from_courses/presentation/pages/sign_in/sign_in_form.dart
@@ -4,45 +4,54 @@ class SignInPage extends StatelessWidget {
   const SignInPage({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
+  Widget build(BuildContext context) => Scaffold(
         body: Stack(
           alignment: Alignment.bottomCenter,
           children: [
             Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.center,
-                    end: Alignment.bottomCenter,
-                    colors: [Color(0xFF2A3B78), Color(0xFF0A063C)],
-                  ),
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.topCenter,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0xFF2A3B78), Color(0xFF0A063C)],
                 ),
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/noostar_entry_logo.png'),
-                        fit: BoxFit.contain),
-                  ),
-                )),
-            FractionallySizedBox(
-              heightFactor: 0.7,
-              child: Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    color: Color(0xFFF1F0F6),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
               ),
             ),
+            Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: SafeArea(
+                    bottom: false,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        'assets/noostar_entry_logo.png',
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFF1F0F6),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
-      ),
-    );
-  }
+      );
 }
